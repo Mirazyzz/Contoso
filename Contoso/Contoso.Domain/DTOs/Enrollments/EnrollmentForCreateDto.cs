b2d54a3e4f65d7f998d63dad9713f6d11350d491
Mirazyzz <khidoyatov1997@gmail.com>
2022-05-18 19:59:23 +0200

    add enrollment dtos

---
+++ Contoso/Contoso.Domain/DTOs/Enrollments/EnrollmentForCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using Contoso.Domain.Enums;
+
+namespace Contoso.Domain.DTOs.Enrollments
+{
+    public class EnrollmentForCreateDto
+    {
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+        public Grade Grade { get; set; }
+
+        public EnrollmentForCreateDto(int studentId, int subjectId, Grade? grade)
+        {
+            StudentId = studentId;
+            SubjectId = subjectId;
+            Grade = grade ?? Enums.Grade.F;
+        }
+    }
+}
---
+++ Contoso/Contoso.Domain/DTOs/Enrollments/EnrollmentForUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using Contoso.Domain.Enums;
+
+namespace Contoso.Domain.DTOs.Enrollments
+{
+    public class EnrollmentForUpdateDto
+    {
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+        public Grade? Grade { get; set; }
+    }
+}
